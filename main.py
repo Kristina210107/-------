ed75169c01756e9cd5d8cee39cf2125f8a9c29b7
Kristina <Kristina.lotkova2101@yandex.ru>
2025-10-11 09:48:12 +0300

    add lis for DB

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from src.api.products import router as products_router
+
 app = FastAPI(title="MyShop", version="0.0.1")
 
-@app.get("/" , tags=["Магазин"])
+
+
+@app.get("/", tags=["Магазин"])
 async def home():
     return {"data" : "Welcome to my Shop"}
+
+app.include_router(products_router)
--- src/api/products.py
+++ src/api/products.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/products", tags = ["Продукы"])
+router = APIRouter(prefix="/products", tags=["Продукты"])
 
 shop_db ={
     0 : {
         "name" : "Шампунь",
         "quantity": 3,
-        "price" : 100
+        "price" : 50
     },
     1 : {
         "name" : "Пицца",
@@ -22,59 +22,48 @@ shop_db ={
 
 id_product = 3
 
-@router.get("/" , tags=["Товары"])
-async def get_products():
+@router.get("/", tags=["Продукты"])
+async def get_produtcs():
     return {"data" : shop_db}
-@router.get("/{id}", tags=["Товары"])
+@router.get("/{id}", tags=["Продукты"])
 async def get_produtc(id: int):
     if shop_db.get(id, None):
         return {"data" : shop_db[id]}
     else:
         return {"msg" : "Товара не существует"}
-@router.post("/" , tags=["Товары"])
-async def add_product(data: dict): 
-    id = len(shop_db.keys())
-
-    shop_db[id] = data  
-    return {"msg" : "Товар добавлен"}
-@router.post("/" , tags=["Товары"])
+@router.post("/", tags=["Продукты"])
 async def add_product(data: dict):
     global id_product
     id: int = id_product
     if shop_db.get(id, None):
-        shop_db[id] = data 
+        shop_db[id] = data
         id_product += 1
-        return{"msg" : "Данные добавлены"}
+        return {"msg" : "Данные добавлены"}
     else:
-        return{"err": "Такой товар существует!"}
-@router.put("/{id}" , tags=["Товары"])
-async def edit_product(id: int, data: dict):
-    if shop_db.get(id, None):
-        shop_db[id] = data 
-        return{"msg" : "Данные добавлены"}
-    else:
-        return{"err": "Такой товар существует!"}
-@router.delete("/{id}" , tags=["Товары"]) 
-async def delete_products(id: int):
-    if shop_db.get(id, None):
-        del shop_db[id]     
-        return{"msg" : f"Товар с id {id} обновлён"}
-    return{"err": "Такой товар  не существует!"}
+        return {"err" : "Такой товар существует!"}
 
-@router.put("/{id}" , tags=["Товары"])
+@router.delete("/{id}", tags=["Продукты"])
+async def delete_product(id: int):
+    if shop_db.get(id, None):
+        del shop_db[id]
+        return {"msg" : f"Товар с id: {id} удален"}
+    return {"err" : "Такой товар не существует!"}
+
+@router.put("/{id}", tags=["Продукты"])
 async def edit_product(id:int, data: dict):
     if shop_db.get(id, None):
         shop_db[id] = data
-        return{"msg" : f"Товар с id {id} обновлён"}
-    return{"err": "Такой товар  не существует!"}
-@router.patch("/{id}" , tags=["Товары"])
+        return {"msg" : f"Товар с id: {id} обновлен"}
+    return {"err" : "Такой товар не существует!"}
+
+@router.patch("/{id}", tags=["Продукты"])
 async def edit_product_partialy(id:int, data: dict):
     if shop_db.get(id, None):
         product = shop_db[id]
-        for k,  v in data.items():
+        for k, v in data.items():
             if product.get(k, None):
                 shop_db[id][k] = v
-
-        return {"msg" : f"Товар с id {id} обновлён"}
+                
+        return {"msg" : f"Товар с id: {id} обновлен"}
     
-    return {"err": "Такой товар  не существует!"}       
+    return {"err" : "Такой товар не существует!"}     
